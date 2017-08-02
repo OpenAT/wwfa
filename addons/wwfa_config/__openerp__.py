3dcabad058a613b4e4e6782c60af891442577e0a
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-02 14:15:27 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/wwfa_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'wwfa_config',
+    'summary': '''FS-Online wwfa instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance wwfa
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
